e73b248d18a666ba6fd2450e9c164d46548b4cf6
Alex Botelho <botelho2305@gmail.com>
2017-11-29 14:17:53 -0500

    Added code from ClientGET.py
    this code currently should be able to pull a resource called /test from a server at 10.0.0.104

--- hubCode.py
+++ hubCode.py
@@ -3,7 +3,26 @@ import logging
 import asyncio
 import aiocoap
 
-contextClient = await Context.create_client_context() #context for sending PUTs and GETs
+async def main():
+  
+  protocol = await Context.create_client_context()
 
-contextServer = await Context. create_server_context() #context for receiving PUTs and GETs
+  request = Message(code=GET, uri='coap://10.0.0.104/test')
+
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print('Result: %s\n%r'%(response.code, response.payload))
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
+  
+  
+  
+#contextClient = await Context.create_client_context() #context for sending PUTs and GETs
+
+#contextServer = await Context. create_server_context() #context for receiving PUTs and GETs
 
