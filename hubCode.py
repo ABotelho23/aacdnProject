2c05a742dcfd3ac5c77c6528fe28aa0e0c45666c
Alex Botelho <botelho2305@gmail.com>
2017-11-29 16:31:24 -0500

    Update hubCode.py
    -Should fully support GET and PUT to 10.0.0.101

--- hubCode.py
+++ hubCode.py
@@ -5,17 +5,30 @@ import aiocoap
 
 async def main():
   
-  protocol = await Context.create_client_context()
-
-  request = Message(code=GET, uri='coap://10.0.0.104/test')
-
+  selection = raw_input("1. GET\n 2. PUT \n\n")
+  
+  if (selection == 1):
+    request = Message(code=GET, uri='coap://10.0.0.101/test')
+  
     try:
-        response = await protocol.request(request).response
+      response = await protocol.request(request).response
     except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
+      print('Failed to fetch resource:')
+      print(e)
     else:
-        print('Result: %s\n%r'%(response.code, response.payload))
+      print('Result: %s\n%r'%(response.code, response.payload))
+  
+  else:
+    payload = raw_input("What is the paylod?")
+    request = Message(code=PUT, payload=payload)
+    # These direct assignments are an alternative to setting the URI like in
+    # the GET example:
+    request.opt.uri_host = '10.0.0.101'
+    request.opt.uri_path = ("test")
+
+    response = await context.request(request).response
+
+    print('Result: %s\n%r'%(response.code, response.payload))
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
