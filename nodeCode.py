6670c2e6710e78b51cf6d1400c850f6666803b44
thekidd54 <33671819+thekidd54@users.noreply.github.com>
2017-11-29 14:22:45 -0500

    Create nodeCode.py
    Skeleton for nodeCode

---
+++ nodeCode.py
@@ -0,0 +1,25 @@
+import datetime
+import logging
+
+import asyncio
+
+import aiocoap.resource as resource
+import aiocoap
+
+
+
+
+def main():
+    # Resource tree creation
+    root = resource.Site()
+
+    root.add_resource(('.well-known', 'core'),
+            resource.WKCResource(root.get_resources_as_linkheader))
+    root.add_resource(('test',), TimeResource())
+
+    asyncio.Task(aiocoap.Context.create_server_context(root))
+
+    asyncio.get_event_loop().run_forever()
+
+if __name__ == "__main__":
+    main()
