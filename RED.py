691357b210ea462422aa19be579e9a786aacd272
Alex Botelho <botelho2305@gmail.com>
2017-11-30 15:58:32 -0500

    Create RED.py
    using this to test light colours

---
+++ RED.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+RED = 17
+GREEN = 18
+BLUE = 27
+
+GPIO.setup(RED,GPIO.OUT)
+GPIO.setup(GREEN,GPIO.OUT)
+GPIO.setup(BLUE,GPIO.OUT)
+
+"""try:
+        while(True):
+                request = input("RGB ->")
+                if (len(request) == 3):
+                        GPIO.output(RED,int(request[0]))
+                        GPIO.output(GREEN,int(request[1]))
+                        GPIO.output(BLUE,int(request[2]))"""
+try:
+  GPIO.output(RED, 0)
+
+except KeyboardInterrupt:
+        GPIO.cleanup()
