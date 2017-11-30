c9c7895e97a6f53276eaace1d748035a24671c98
Alex Botelho <botelho2305@gmail.com>
2017-11-30 16:10:25 -0500

    Create BLUE.py

---
+++ BLUE.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+RED = 17
+GREEN = 18
+BLUE = 27
+
+GPIO.setup(RED,GPIO.OUT)
+GPIO.setup(GREEN,GPIO.OUT)
+GPIO.setup(BLUE,GPIO.OUT)
+GPIO.output(RED,1)
+GPIO.output(GREEN,1)
+GPIO.output(BLUE,1)
+"""try:
+        while(True):
+                request = input("RGB ->")
+                if (len(request) == 3):
+                        GPIO.output(RED,int(request[0]))
+                        GPIO.output(GREEN,int(request[1]))
+                        GPIO.output(BLUE,int(request[2]))"""
+try:
+        while(True):
+                flub = input("Will turn BLUE on any input")
+                GPIO.output(BLUE,0)
+
+except KeyboardInterrupt:
+        GPIO.cleanup()
