172036f0bcb9f7f279df8cdabaec65ea3bee7453
thekidd54 <33671819+thekidd54@users.noreply.github.com>
2018-01-16 16:28:39 -0500

    Reduced print output

--- blindStatus.py
+++ blindStatus.py
@@ -1,14 +1,12 @@
 
 def checkStatus():
 
-  print("Checking Status File")
   readPath = '/home/pi/BlindStatus.txt'
   statusFile = open(readPath, 'r')
   
   currentStatus = statusFile.readline()
   print("Current Status: ")
   print(currentStatus)
-
 
 if __name__ == "__main__":
     checkStatus()
