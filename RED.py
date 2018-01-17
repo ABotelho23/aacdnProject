da2f5c3d91290a0ecadd1cb8ab815a263aa5a9bc
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-01-17 16:10:54 -0500

    Update RED.py

--- RED.py
+++ RED.py
@@ -11,20 +11,20 @@ def redOn():
  GPIO.setup(RED,GPIO.OUT)
  GPIO.setup(GREEN,GPIO.OUT)
  GPIO.setup(BLUE,GPIO.OUT)
- GPIO.output(RED,1)
- GPIO.output(GREEN,1)
- GPIO.output(BLUE,1)
+ GPIO.output(RED,0)
+ GPIO.output(GREEN,0)
+ GPIO.output(BLUE,0)
 
    
  
  
- # try:
- #       while(True):
-  #              request = input("RGB ->")
-   #             if (len(request) == 3):
-    #                    GPIO.output(RED,int(request[0]))
-     #                   GPIO.output(GREEN,int(request[1]))
-      #                  GPIO.output(BLUE,int(request[2]))
+ try:
+        while(True):
+                request = input("RGB ->")
+                if (len(request) == 3):
+                        GPIO.output(RED,int(request[0]))
+                        GPIO.output(GREEN,int(request[1]))
+                        GPIO.output(BLUE,int(request[2]))
  #try:
   #      while(True):
    #             flub = input("Will turn RED on any input")
