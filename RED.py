472440e40e066f4a94bf22dfd8cb89c3b70c9ced
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-01-17 16:18:20 -0500

    Update RED.py

--- RED.py
+++ RED.py
@@ -2,18 +2,18 @@ import RPi.GPIO as GPIO
 
 def redOn():
 
-GPIO.setmode(GPIO.BCM)
+ GPIO.setmode(GPIO.BCM)
 
-RED = 17
-GREEN = 18
-BLUE = 27
+ RED = 17
+ GREEN = 18
+ BLUE = 27
 
-GPIO.setup(RED,GPIO.OUT)
-GPIO.setup(GREEN,GPIO.OUT)
-GPIO.setup(BLUE,GPIO.OUT)
-GPIO.output(RED,1)
-GPIO.output(GREEN,1)
-GPIO.output(BLUE,1)
+ GPIO.setup(RED,GPIO.OUT)
+ GPIO.setup(GREEN,GPIO.OUT)
+ GPIO.setup(BLUE,GPIO.OUT)
+ GPIO.output(RED,1)
+ GPIO.output(GREEN,1)
+ GPIO.output(BLUE,1)
 
    
  
