5fd567c14abdca8aa9a05485c6c984a5e9f807b3
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-01-17 16:36:50 -0500

    Update RED.py

--- RED.py
+++ RED.py
@@ -30,7 +30,7 @@ def redOn():
    #             flub = input("Will turn RED on any input")
     #            GPIO.output(RED,0)
 
- except KeyboardInterrupt:
+except KeyboardInterrupt:
         GPIO.cleanup()
 
  #if __name__ == "__main__":
