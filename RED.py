32bea45aa671fb1d368c6a97f497b0c5b658dc43
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-01-17 17:01:27 -0500

    Update RED.py

--- RED.py
+++ RED.py
@@ -1,36 +1,27 @@
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-
-RED = 17
-GREEN = 18
-BLUE = 27
-
-GPIO.setup(RED,GPIO.OUT)
-GPIO.setup(GREEN,GPIO.OUT)
-GPIO.setup(BLUE,GPIO.OUT)
-GPIO.output(RED,False)
-GPIO.output(GREEN,False)
-GPIO.output(BLUE,False)
-
-try:         
-    while True:
-        UserInput = raw_input()
-	UserInput = str(UserInput)
-        if UserInput == "red":
-            GPIO.output(RED, True)
-            GPIO.output(GREEN, False)
-            GPIO.output(BLUE, False)
-        elif UserInput == "green":
-            GPIO.output(RED, False)
-            GPIO.output(GREEN, True)
-            GPIO.output(BLUE, False)
-        elif UserInput == "blue":
-            GPIO.output(RED, False)
-            GPIO.output(GREEN, False)
-            GPIO.output(BLUE, True)
-	else:
-		print("Only red, green, and blue are valid colors.")
-
-except KeyboardInterrupt:
-        GPIO.cleanup()
+import RPi.GPIO as GPIO            # import RPi.GPIO module  
+from time import sleep             # lets us have a delay  
+GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
+GPIO.setup(17, GPIO.OUT)           # set GPIO24 as an output   
+  
+try:  
+    while True:  
+        GPIO.output(17, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+        sleep(0.5)                 # wait half a second  
+        GPIO.output(17, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        sleep(0.5)                 # wait half a second  
+  
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+    GPIO.cleanup() import RPi.GPIO as GPIO            # import RPi.GPIO module  
+from time import sleep             # lets us have a delay  
+GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
+GPIO.setup(17, GPIO.OUT)           # set GPIO24 as an output   
+  
+try:  
+    while True:  
+        GPIO.output(17, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+        sleep(0.5)                 # wait half a second  
+        GPIO.output(17, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        sleep(0.5)                 # wait half a second  
+  
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+    GPIO.cleanup() 
