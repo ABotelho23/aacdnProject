08277abbea748bd08788336301a8ce5c7239c835
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-01-17 17:27:34 -0500

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -2,32 +2,18 @@ import RPi.GPIO as GPIO
 
 def greenOn():
        
- GPIO.setmode(GPIO.BCM)
+  GPIO.setmode(GPIO.BCM)
+  
+  RED = 17
+  GREEN = 18
+  BLUE = 27
+  GPIO.setup(RED,GPIO.OUT)
+  GPIO.setup(GREEN,GPIO.OUT)
+  GPIO.setup(BLUE,GPIO.OUT)
+  GPIO.output(RED,GPIO.HIGH)
+  GPIO.output(GREEN,GPIO.LOW)
+  GPIO.output(BLUE,GPIO.HIGH)
+  
+if __name__ == "__main__":
+  greenOn()
 
- RED = 17
- GREEN = 18
- BLUE = 27
-
- GPIO.setup(RED,GPIO.OUT)
- GPIO.setup(GREEN,GPIO.OUT)
- GPIO.setup(BLUE,GPIO.OUT)
- GPIO.output(RED,1)
- GPIO.output(GREEN,1)
- GPIO.output(BLUE,1)
- """try:
-        while(True):
-                request = input("RGB ->")
-                if (len(request) == 3):
-                        GPIO.output(RED,int(request[0]))
-                        GPIO.output(GREEN,int(request[1]))
-                        GPIO.output(BLUE,int(request[2]))"""
- try:
-        while(True):
-                flub = input("Will turn GREEN on any input")
-                GPIO.output(GREEN,0)
-
- except KeyboardInterrupt:
-        GPIO.cleanup()
-
- if __name__ == "__main__":
-    greenOn()
