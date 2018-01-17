5b7a96e1961e6bda1e7d00d690566b92fd0d147c
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-01-17 16:49:32 -0500

    Update RED.py

--- RED.py
+++ RED.py
@@ -1,37 +1,25 @@
 import RPi.GPIO as GPIO
 
-def redOn():
+GPIO.setmode(GPIO.BCM)
 
- GPIO.setmode(GPIO.BCM)
+RED = 17
+GREEN = 18
+BLUE = 27
 
- RED = 17
- GREEN = 18
- BLUE = 27
+GPIO.setup(RED,GPIO.OUT)
+GPIO.setup(GREEN,GPIO.OUT)
+GPIO.setup(BLUE,GPIO.OUT)
+GPIO.output(RED,0)
+GPIO.output(GREEN,0)
+GPIO.output(BLUE,0)
 
- GPIO.setup(RED,GPIO.OUT)
- GPIO.setup(GREEN,GPIO.OUT)
- GPIO.setup(BLUE,GPIO.OUT)
- GPIO.output(RED,1)
- GPIO.output(GREEN,0)
- GPIO.output(BLUE,0)
-
-   
- 
- 
- #try:
-  #      while(True):
-   #             request = input("RGB ->")
-    #            if (len(request) == 3):
-     #                   GPIO.output(RED,int(request[0]))
-      #                  GPIO.output(GREEN,int(request[1]))
-       #                 GPIO.output(BLUE,int(request[2]))
- #try:
-  #      while(True):
-   #             flub = input("Will turn RED on any input")
-    #            GPIO.output(RED,0)
+try:
+        while(True):
+                request = raw_input("RGB ->")
+                if (len(request) == 3):
+                        GPIO.output(RED,int(request[0]))
+                        GPIO.output(GREEN,int(request[1]))
+                        GPIO.output(BLUE,int(request[2]))
 
 except KeyboardInterrupt:
         GPIO.cleanup()
-
- #if __name__ == "__main__":
-  #  redOn()
