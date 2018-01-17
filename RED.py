3bed1a33a1ae1ceb8e7ccdaa23b8f460d368f6c1
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-01-17 17:03:49 -0500

    Update RED.py

--- RED.py
+++ RED.py
@@ -1,27 +1,17 @@
-import RPi.GPIO as GPIO            # import RPi.GPIO module  
-from time import sleep             # lets us have a delay  
-GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(17, GPIO.OUT)           # set GPIO24 as an output   
-  
-try:  
-    while True:  
-        GPIO.output(17, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
-        sleep(0.5)                 # wait half a second  
-        GPIO.output(17, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-        sleep(0.5)                 # wait half a second  
-  
-except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
-    GPIO.cleanup() import RPi.GPIO as GPIO            # import RPi.GPIO module  
-from time import sleep             # lets us have a delay  
-GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(17, GPIO.OUT)           # set GPIO24 as an output   
-  
-try:  
-    while True:  
-        GPIO.output(17, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
-        sleep(0.5)                 # wait half a second  
-        GPIO.output(17, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-        sleep(0.5)                 # wait half a second  
-  
-except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
-    GPIO.cleanup() 
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+RED = 17
+GREEN = 18
+BLUE = 27
+
+GPIO.setup(RED,GPIO.OUT)
+GPIO.output(RED,GPIO.LOW)
+GPIO.setup(GREEN,GPIO.OUT)
+GPIO.output(GREEN,GPIO.HIGH)
+GPIO.setup(BLUE,GPIO.OUT)
+GPIO.output(BLUE,GPIO.HIGH)
+
+except KeyboardInterrupt:
+        GPIO.cleanup()
