00feb48ea6f6f1beb542f644df50a9205a0c287b
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-01-17 17:29:29 -0500

    Create MAGENTA.py

---
+++ MAGENTA.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+
+def magentaOn():
+
+  GPIO.setmode(GPIO.BCM)
+  
+  RED = 17
+  GREEN = 18
+  BLUE = 27
+  GPIO.setup(RED,GPIO.OUT)
+  GPIO.setup(GREEN,GPIO.OUT)
+  GPIO.setup(BLUE,GPIO.OUT)
+  GPIO.output(RED,GPIO.LOW)
+  GPIO.output(GREEN,GPIO.LOW)
+  GPIO.output(BLUE,GPIO.HIGH)
+  
+if __name__ == "__main__":
+  magentaOn()
