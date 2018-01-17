d26ba19096b6ac103330b45238dbeb0b4418451a
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-01-17 15:39:30 -0500

    Update BLUE.py

--- BLUE.py
+++ BLUE.py
@@ -1,23 +1,7 @@
 import RPi.GPIO as GPIO
 
 def blueOn():
-
-  GPIO.setmode(GPIO.BCM)
-    
-    global RED
-    global GREEN
-    global BLUE
-    
-    RED = 17
-    GREEN = 18
-    BLUE = 27
-
-    GPIO.setup(RED,GPIO.OUT)
-    GPIO.setup(GREEN,GPIO.OUT)
-    GPIO.setup(BLUE,GPIO.OUT)
-        
-        
- 
+         
  GPIO.setmode(GPIO.BCM)
 
  RED = 17
