f865c001a3a1f2447f375b495933db5d79e16fb2
ashproject <33730158+ashproject@users.noreply.github.com>
2018-01-19 17:45:58 -0500

    Create camera1.py
    capture picture

---
+++ camera1.py
@@ -0,0 +1,39 @@
+import picamera
+from subprocess import call
+from datetime import datetime
+from time import sleep
+
+
+def main()
+# File path
+  filePath = "/mnt/captures/"
+  picTotal = 3
+  picCount = 0
+
+  while picCount < picTotal:
+    # Grab the current time
+      cTime = datetime.now()
+   
+      picT = cTime.strftime("%Y.%m.%d-%H%M%S")
+      picName = picT + '.jpg'
+      FilePathPic = filePath + picName
+
+    # Take picture
+      with picamera.PiCamera() as camera:
+          camera.resolution = (1280,720)
+          camera.capture(FilePathPic)
+          print("picture taken.")
+
+    #currenttime variable
+      timestampMessage = cTime.strftime("%Y.%m.%d - %H:%M:%S")
+
+    #time stamp command 
+     timestampCommand = "/mnt/captures/convert/ " + FilePathPic + " -pointsize 36 \
+      -fill blue -annotate +700+650 '" + timestampMessage + "' " + FilePathPic
+    
+      call([timestampCommand], shell=True)
+     print("  picture captured")
+
+
+     picCount += 1
+     sleep(3)
