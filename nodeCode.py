520febc55200e071a060839cebfd3a4a24e63c0d
Alex Botelho <botelho2305@gmail.com>
2018-01-19 18:09:15 -0500

    Added shell for video resource
    Still needs to call proper video script

--- nodeCode.py
+++ nodeCode.py
@@ -28,6 +28,27 @@ class TakePicture(resource.Resource):
         self.set_content(request.payload)"""
         capturePicture.main()
         return aiocoap.Message(code=aiocoap.CHANGED, payload=b'Picture captured.')
+    
+class TakeVideo(resource.Resource):
+    """This is our first resource defined from scratch to test functionality."""
+
+    def __init__(self):
+        super().__init__()
+        self.set_content(b"If you perform a PUT on this URI, camera will take a pvideo of the amount of seconds you send.")
+
+    def set_content(self, content):
+        self.content = content
+           
+    #this is the render for a GET. This returns the payload.
+    async def render_get(self, request):
+        return aiocoap.Message(payload=self.content)
+    
+    #this is the render for a PUT. This sets what the resource value is.
+    async def render_put(self, request):
+        """print('PUT payload: %s' % request.payload)
+        self.set_content(request.payload)"""
+        '''capturePicture.main()'''
+        return aiocoap.Message(code=aiocoap.CHANGED, payload=b'Video captured.')
 # logging setup
 
 logging.basicConfig(level=logging.INFO)
@@ -40,6 +61,7 @@ def main():
     root.add_resource(('.well-known', 'core'),
             resource.WKCResource(root.get_resources_as_linkheader))
     root.add_resource(('node2','camera','capture'), TakePicture())
+    root.add_resource(('node2','camera','captureVideo'), TakeVideo())
 
     asyncio.Task(aiocoap.Context.create_server_context(root))
 
