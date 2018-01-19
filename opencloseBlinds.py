39be5cb15226ee6efeccf7f150fc8c88171bc304
thekidd54 <33671819+thekidd54@users.noreply.github.com>
2018-01-19 12:20:20 -0500

    modified main function

--- opencloseBlinds.py
+++ opencloseBlinds.py
@@ -62,6 +62,25 @@ def writeStatus(num):
   statusFile.close()
   
 def main(fromServer):
+  
+   GPIO.setmode(GPIO.BOARD)
+
+  Motor1A = 16
+  Motor1B = 18
+  Motor1E = 22
+
+  GPIO.setup(Motor1A,GPIO.OUT)
+  GPIO.setup(Motor1B,GPIO.OUT)
+  GPIO.setup(Motor1E,GPIO.OUT)
+
+  #Amount of time to fully open/close
+  fullTime = 5
+  #Step incrememter 
+  stepCounter = 10
+  #Adjust time accordinly
+  finalTime = 0
+
+  pwmtest = GPIO.PWM(Motor1E,100)
 
   #Check what is the argument
   print(fromServer)
@@ -71,6 +90,8 @@ def main(fromServer):
   elif (fromServer == '10'):
     closeStart(fullTime)
     writeStatus(fromServer)
+    
+  GPIO.cleanup()
 
 
 if __name__ == "__main__":
