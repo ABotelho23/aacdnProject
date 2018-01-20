67d8131ddf0b7ba6bde020053db4d660d6027a1f
thekidd54 <33671819+thekidd54@users.noreply.github.com>
2018-01-19 23:00:32 -0500

    Cleaned up code & spacing

--- blindStatus.py
+++ blindStatus.py
@@ -1,12 +1,8 @@
-
 def checkStatus():
 
   readPath = '/home/pi/BlindStatus.txt'
   statusFile = open(readPath, 'r')
-  
   currentStatus = statusFile.readline()
-  #print(currentStatus)
-  
   statusFile.close()
   
   return currentStatus
