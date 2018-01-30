a8ef84affd568620935c241f31a3232463e0ed52
thekidd54 <33671819+thekidd54@users.noreply.github.com>
2018-01-30 16:06:18 -0500

    Added Main function
    Video will be captured for 5 seconds, saved as .h264 and then converted to .mp4
    both will be saved on the samba share (on the hub)

--- captureVideo.py
+++ captureVideo.py
@@ -1,32 +1,27 @@
 import picamera
 from time import sleep
-from subprocess import call
 import datetime as dt
 import os
-import shlex
 import subprocess
 
-#File path 
-filePath = "/mnt/captures/"
-fileName = os.path.join(filePath, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S'))
-fileWithExtension = fileName
-fileWithExtension += '.h264'
+def main()
+    #File path 
+    filePath = "/mnt/captures/"
+    fileName = os.path.join(filePath, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S'))
+    fileWithExtension = fileName
+    fileWithExtension += '.h264'
 
-#  camera Setup
-with picamera.PiCamera() as camera:
-    camera.start_recording(fileWithExtension)
-    sleep(5)
-    camera.stop_recording()
+    #Camera Setup
+    with picamera.PiCamera() as camera:
+        camera.start_recording(fileWithExtension)
+        sleep(5)
+        camera.stop_recording()
 
-print("convert the video.")
+    print("convert the video.")
 
-#command = shlex.split("MP4Box -add {f}.h264 {f}.mp4".format(f=str(fileName)))
-command = "MP4Box -add {f}.h264 {f}.mp4".format(f=str(fileName))
-try:
-    output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-except subprocess.CalledProcessError as e:
-    print('FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output))
-
-#call([command], shell=True)
-# Video converted.
-print("Video converted.")
+    command = "MP4Box -add {f}.h264 {f}.mp4".format(f=str(fileName))
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except subprocess.CalledProcessError as e:
+        print('FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output))
+    print("Video converted.")
