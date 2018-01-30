b72ec573c814648bd9e0ce8cf16c1c18a08eb6fe
ashproject <33730158+ashproject@users.noreply.github.com>
2018-01-30 13:44:39 -0500

    Create captureVideo.py
    capture Video

---
+++ captureVideo.py
@@ -0,0 +1,16 @@
+import picamera
+from time import sleep
+from subprocess import call
+
+#  camera Setup
+with picamera.PiCamera() as camera:
+    camera.start_recording("pythonVideo.h264")
+    sleep(5)
+    camera.stop_recording()
+
+print("convert the video.")
+
+command = "MP4Box -add pythonVideo.h264 convertedVideo.mp4"
+call([command], shell=True)
+# Video converted.
+print("Video converted.")
