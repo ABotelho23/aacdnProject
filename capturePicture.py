c594a6d72b7ce64c476046175def911b1dd710ff
thekidd54 <33671819+thekidd54@users.noreply.github.com>
2018-01-30 17:03:06 -0500

    Added argument into main

--- capturePicture.py
+++ capturePicture.py
@@ -2,20 +2,19 @@ import picamera
 from subprocess import call
 from datetime import datetime
 from time import sleep
-#New
 from picamera import Color
 
-def main():
+def main(fromServer):
 # File path
   filePath = "/mnt/captures/"
-  picTotal = 3
+  picTotal = fromServer
   picCount = 0
 
   while picCount < picTotal:
     #Current time
     cTime = datetime.now()
    
-    picT = cTime.strftime("%Y.%m.%d-%H%M%S")
+    picT = cTime.strftime("%Y.%m.%d-%H:%M:%S")
     picName = picT + '.jpg'
     FilePathPic = filePath + picName
 
@@ -29,17 +28,6 @@ def main():
       camera.annotate_text = picT
       camera.capture(FilePathPic)
       print("picture taken.")
-
-      #currenttime variable
-    timestampMessage = cTime.strftime("%Y.%m.%d - %H:%M:%S")
-    
-      #time stamp command 
-    timestampCommand = FilePathPic + " -pointsize 36 \
-    -fill blue -annotate +700+650 '" + timestampMessage + "' " #+ FilePathPic
-    
-    call([timestampCommand], shell=True)
-    print("  picture captured")
-
 
     picCount += 1
     sleep(3)
