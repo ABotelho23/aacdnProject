1866a5bd2d7f6518b008af5af91edd62ecb32969
ashproject <33730158+ashproject@users.noreply.github.com>
2018-02-02 14:13:22 -0500

    time stamp
    time stamp changed

--- capturePicture.py
+++ capturePicture.py
@@ -24,7 +24,7 @@ def main(fromServer):
       camera.resolution = (1280,720)
      
       #Timestamp
-      camera.annotate_foreground = Color('black')
+      camera.annotate_foreground +700+650 = Color('black')
       camera.annotate_background = Color('white')
       camera.annotate_text = picT
       camera.capture(FilePathPic)
