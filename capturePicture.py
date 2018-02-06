4300112e618b14166e6b33e7c0d7453495a5ea2b
ashproject <33730158+ashproject@users.noreply.github.com>
2018-02-06 13:51:20 -0500

    Update capturePicture.py

--- capturePicture.py
+++ capturePicture.py
@@ -4,32 +4,32 @@ from datetime import datetime
 from time import sleep
 from picamera import Color
 
-def main(fromServer):
+#def main(fromServer):
 # File path
-  filePath = "/mnt/captures/"
-  picTotal = fromServer
-  intpicTotal = int(picTotal)
-  picCount = 0
+  #filePath = "/mnt/captures/"
+  #picTotal = fromServer
+  #intpicTotal = int(picTotal)
+  #picCount = 0
 
-  while picCount < intpicTotal:
+  #while picCount < intpicTotal:
     #Current time
-    cTime = datetime.now()
+    #cTime = datetime.now()
    
-    picT = cTime.strftime("%Y-%m-%d %H:%M:%S")
-    picName = picT + '.jpg'
-    FilePathPic = filePath + picName
+    #picT = cTime.strftime("%Y-%m-%d %H:%M:%S")
+    #picName = picT + '.jpg'
+   #FilePathPic = filePath + picName
 
     # Take picture
-    with picamera.PiCamera() as camera:
-      camera.resolution = (1280,720)
+    #with picamera.PiCamera() as camera:
+      #camera.resolution = (1280,720)
      
       #Timestamp
       #camera.annotate = 700+650
-      camera.annotate_foreground = Color('black')
-      camera.annotate_background = Color('white')
-      camera.annotate_text = picT
-      camera.capture(FilePathPic)
-      print("picture taken.")
+      #camera.annotate_foreground = Color('black')
+      #camera.annotate_background = Color('white')
+      #camera.annotate_text = picT
+      #camera.capture(FilePathPic)
+      #print("picture taken.")
 
-    picCount += 1
-    sleep(3)
+    #picCount += 1
+    #sleep(3)
