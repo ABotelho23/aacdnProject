f400438834f2b5403a615dd47a70c50d6376d875
ashproject <33730158+ashproject@users.noreply.github.com>
2018-02-07 17:50:35 -0500

    Update capturePicture.py

--- capturePicture.py
+++ capturePicture.py
@@ -32,7 +32,7 @@ while picCount < intpicTotal:
     camera.resolution = (1280,720)
      
       #Timestamp
-     camera.annotate_foreground = Color('black')
+    camera.annotate_foreground = Color('black')
      camera.annotate_background = Color('white')
      camera.annotate_text = picT
      camera.capture(FilePathPic)
