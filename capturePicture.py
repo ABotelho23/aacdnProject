c4b5af772cd76964b6336166a325b278abebe32d
ashproject <33730158+ashproject@users.noreply.github.com>
2018-02-07 18:43:45 -0500

    Update capturePicture.py

--- capturePicture.py
+++ capturePicture.py
@@ -1,3 +1,42 @@
+
+import picamera
+ from subprocess import call
+ from datetime import datetime
+ from time import sleep
+ from picamera import Color
+ 
+ def main(fromServer):
+ # File path
+   filePath = "/mnt/captures/"
+   picTotal = fromServer
+   intpicTotal = int(picTotal)
+   picCount = 0
+ 
+   while picCount < intpicTotal:
+     #Current time
+      cTime = datetime.now()
+     
+      picT = cTime.strftime("%Y-%m-%d %H:%M:%S")
+      picName = cTime + '.jpg'
+      picName = picT + '.jpg'
+      FilePathPic = filePath + picName
+  
+      # Take picture
+     with picamera.PiCamera() as camera:
+       camera.resolution = (1280,720)
+      
+       #Timestamp
+       #camera.annotate = 700+650
+       camera.annotate_foreground = Color('black')
+       camera.annotate_background = Color('white')
+       camera.annotate_text = picT
+       camera.capture(FilePathPic)
+       print("picture taken.")
+ 
+     picCount += 1
+     sleep(3)
+
+"""
 import picamera
 from subprocess import call
 from datetime import datetime
@@ -38,7 +77,7 @@ while picCount < intpicTotal:
     print("picture taken.")
       
       
-"""
+
 while True:
     motionState = p3picamera.motion()
     print(motionState)
@@ -46,6 +85,7 @@ while True:
         cTime = getTime()
         picName = captureImage(cTime, picPath)
         timeStamp(currentTime, picPath, picName)
-"""
+
 picCount += 1
 sleep(3)
+"""
