2e4f7aee12649906823d383f213cccfb1ade0f1c
ashproject <33730158+ashproject@users.noreply.github.com>
2018-02-08 11:34:13 -0500

    Update capturePicture.py

--- capturePicture.py
+++ capturePicture.py
@@ -3,20 +3,6 @@ import picamera
 import picammotion
 from subprocess import call
 from datetime import datetime
-from time import sleep
-from picamera import Color
-
-
-def main(fromServer):
-    # File path
-    filePath = "/mnt/captures/"
-    picTotal = fromServer
-    intpicTotal = int(picTotal)
-    picCount = 0
-import picamera
-import picammotion
-from subprocess import call
-from datetime import datetime
 from datetime import timedelta
 from time import sleep
 from picamera import Color
@@ -24,7 +10,7 @@ from picamera import Color
 
 def main(fromServer):
     # File path
-    filePath = "/mnt/captures/"
+    filePath = "/home/pi/captureStorage/"
     picTotal = fromServer
     intpicTotal = int(picTotal)
     picCount = 0
@@ -43,7 +29,8 @@ def main(fromServer):
         
         motionState = picammotion.motion()
         print(motionState)
-        if motionState: 
+        if motionState:
+            currentTime = getTime()  
             # Take picture
             with picamera.PiCamera() as camera:
                 camera.resolution = (1280,720)
@@ -59,4 +46,56 @@ def main(fromServer):
                 picCount += 1
                 sleep(3)
                 
-              
+                """
+                import picamera
+                from subprocess import call
+                from datetime import datetime
+                from time import sleep
+                from picamera import Color
+                import io 
+                #import P3picam
+                
+                
+                
+                motionState = False
+                picPath = "/mnt/captures"
+                
+                def main(fromServer):  
+                    # File path
+                    filePath = "/mnt/captures/"
+                    picTotal = fromServer
+                    intpicTotal = int(picTotal)
+                    picCount = 0
+                    
+                    while picCount < intpicTotal:
+                        #Current time
+                        cTime = datetime.now()
+                        
+                        picT = cTime.strftime("%Y-%m-%d %H:%M:%S")
+                        picName = picT + '.jpg'
+                        FilePathPic = filePath + picName
+                        
+                        # Take picture
+                        with picamera.PiCamera() as camera:
+                            camera.resolution = (1280,720)
+                            
+                            #Timestamp
+                            camera.annotate_foreground = Color('black')
+                            camera.annotate_background = Color('white')
+                            camera.annotate_text = picT
+                            camera.capture(FilePathPic)
+                            print("picture taken.")
+                            
+                            
+                            
+                            while True:
+                                motionState = p3picamera.motion()
+                                print(motionState)
+                                if motionState:
+                                    cTime = getTime()
+                                    picName = captureImage(cTime, picPath)
+                                    timeStamp(currentTime, picPath, picName)
+                                    
+                                    picCount += 1
+                                    sleep(3)
+                                    """
