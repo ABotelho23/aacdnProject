f8f20858b0217252dd587ed80dc63d16aae0897f
ABotelho23 <botelho2305@gmail.com>
2018-02-09 16:10:55 -0500

    initial commits for adding motion capture notifications sent to hub

--- nodeCode.py
+++ nodeCode.py
@@ -9,6 +9,22 @@ import aiocoap
 import capturePicture
 import captureVideo
 
+class MotionThread(threading.Thread):
+    def run(self):
+
+
+
+
+        targetURI = 'coap://' + '10.0.0.100' + '/cameras/capture'
+        context = await Context.create_client_context()
+
+        #DEFINE WHAT THE PAYLOAD IS
+        payload = b'PAYLOAD CONTENT'
+        request = Message(code=PUT, uri=targetURI, payload=payload)
+        print('Sending payload')
+        response = await context.request(request).response
+        print('Result: %s\n%r'%(response.code, response.payload))
+
 class TakePicture(resource.Resource):
     """This is our first resource defined from scratch to test functionality."""
 
@@ -18,11 +34,11 @@ class TakePicture(resource.Resource):
 
     def set_content(self, content):
         self.content = content
-           
+
     #this is the render for a GET. This returns the payload.
     async def render_get(self, request):
         return aiocoap.Message(payload=self.content)
-    
+
     #this is the render for a PUT. This sets what the resource value is.
     async def render_put(self, request):
         """print('PUT payload: %s' % request.payload)
@@ -30,7 +46,7 @@ class TakePicture(resource.Resource):
         #Call Take picture function
         takePicture(request.payload)
         return aiocoap.Message(code=aiocoap.CHANGED, payload=b'Picture captured.')
-    
+
 class TakeVideo(resource.Resource):
     """This is our first resource defined from scratch to test functionality."""
 
@@ -40,11 +56,11 @@ class TakeVideo(resource.Resource):
 
     def set_content(self, content):
         self.content = content
-           
+
     #this is the render for a GET. This returns the payload.
     async def render_get(self, request):
         return aiocoap.Message(payload=self.content)
-    
+
     #this is the render for a PUT. This sets what the resource value is.
     async def render_put(self, request):
         """print('PUT payload: %s' % request.payload)
@@ -57,12 +73,18 @@ logging.basicConfig(level=logging.INFO)
 logging.getLogger("coap-server").setLevel(logging.DEBUG)
 
 def takePicture(hubRequest):
-    
+
     print(hubRequest)
     hubRequeststr = hubRequest.decode()
     capturePicture.main(hubRequeststr)
 
 def main():
+
+    #start motion detection thread
+
+    moDetectionThread = MotionThread()
+        moDetectionThread.start()
+
     # Resource tree creation
     root = resource.Site()
 
