3460310a1b368c114cf2a98c283a657732c7bb02
ashproject <33730158+ashproject@users.noreply.github.com>
2018-02-09 17:02:13 -0500

    Update nodeCode.py

--- nodeCode.py
+++ nodeCode.py
@@ -20,7 +20,7 @@ class MotionThread(threading.Thread):
                 
                
         targetURI = 'coap://' + '10.0.0.100' + '/cameras/capture'
-       
+        context = await Context.create_client_context()
         
 
         #DEFINE WHAT THE PAYLOAD IS
