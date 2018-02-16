c0bb0bf6272ca4e49675c88e04d32d22f3eb70c7
ashproject <33730158+ashproject@users.noreply.github.com>
2018-02-16 11:23:29 -0500

    Update captureVideo.py
    motion detection  line 28

--- captureVideo.py
+++ captureVideo.py
@@ -24,23 +24,28 @@ def main(fromServer):
         buffer = im.load()
         imageData.close()
         return im, buffer
-        motionState = picammotion.motion()
-        print(motionState)
-        if motionState:
-            #Camera Setup
-            with picamera.PiCamera() as camera:
-                camera.start_recording(fileWithExtension)
-                sleep(5)
-                camera.stop_recording()
-                
-                print("convert the video.")
-                
-                command = "MP4Box -add {f}.h264 {f}.mp4".format(f=str(fileName))
-                try:
-                    output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-                except subprocess.CalledProcessError as e:
-                    print('FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output))
-                    print("Video converted.")
+    
+        cTime = datetime.now()
+        dtime + cTime + timedelta(seconds=intpicTotal)
+        while cTime < dTime:
+    
+			motionState = picammotion.motion()
+			print(motionState)
+			if motionState:
+				#Camera Setup
+				with picamera.PiCamera() as camera:
+					camera.start_recording(fileWithExtension)
+					sleep(5)
+					camera.stop_recording()
+
+					print("convert the video.")
+
+					command = "MP4Box -add {f}.h264 {f}.mp4".format(f=str(fileName))
+					try:
+						output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+					except subprocess.CalledProcessError as e:
+						print('FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output))
+						print("Video converted.")
 
 
 
