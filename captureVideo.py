6ecafcbaf3763de134822fccc12ab7a55532ebc8
ashproject <33730158+ashproject@users.noreply.github.com>
2018-02-21 17:58:13 -0500

    Update captureVideo.py

--- captureVideo.py
+++ captureVideo.py
@@ -19,27 +19,27 @@ def main(fromServer):
 
 	cTime = dt.datetime.now()
 	dTime = cTime + timedelta(seconds=10)  # constant time for motion detection to stay on
-	while cTime < dTime:
+	#while cTime < dTime:
 		
-		cTime = dt.datetime.now()
-		fileName = os.path.join(filePath, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S'))
-		motionState = picammotion.motion()
-		print(motionState)
-		if motionState:
-			#Camera Setup
-			with picamera.PiCamera() as camera:
-				camera.start_recording(fileName + '.h264')
-				sleep(intpicTotal)  # payload specfying how long to record in seconds
-				camera.stop_recording()
+	cTime = dt.datetime.now()
+	fileName = os.path.join(filePath, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S'))
+	#motionState = picammotion.motion()
+	#print(motionState)
+#	if motionState:
+		#Camera Setup
+	with picamera.PiCamera() as camera:
+		camera.start_recording(fileName + '.h264')
+		sleep(intpicTotal)  # payload specfying how long to record in seconds
+		camera.stop_recording()
 
-				print("convert the video.")
+		print("convert the video.")
 
-				command = "MP4Box -add {f}.h264 {f}.mp4".format(f=str(fileName))
-				try:
-					output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-				except subprocess.CalledProcessError as e:
-					print('FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output))
-					print("Video converted.")
+		command = "MP4Box -add {f}.h264 {f}.mp4".format(f=str(fileName))
+		try:
+			output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+		except subprocess.CalledProcessError as e:
+			print('FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output))
+			print("Video converted.")
 
 
 
