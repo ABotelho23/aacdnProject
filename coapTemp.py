23f40933a0cebd27a19ac7408df99ad54c5452cb
ABotelho23 <botelho2305@gmail.com>
2018-03-02 17:43:38 -0500

    Added context code

--- coapTemp.py
+++ coapTemp.py
@@ -8,3 +8,19 @@ import aiocoap.resource as resource
 from aiocoap import *
 import aiocoap
 
+def main():
+    targetURI = 'coap://10.0.0.103/thermo/temp'
+    protocol = await Context.create_client_context()
+
+    request = Message(code=GET, uri=targetURI)
+
+    try:
+      response = await protocol.request(request).response
+    except Exception as e:
+      print('Failed to fetch resource:')
+      print(e)
+      return "ERROR BLAHHHHHHH"
+    else:
+      print('Result: %s\n%r'%(response.code, response.payload))
+
+      return response.payload
