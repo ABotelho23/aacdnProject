9d29fc3691e629d174046389ecb8df0706debe78
thekidd54 <33671819+thekidd54@users.noreply.github.com>
2018-03-02 18:51:52 -0500

    Added if __name__ == "__main__":

--- coapTemp.py
+++ coapTemp.py
@@ -31,3 +31,6 @@ async def main():
       print('Result: %s\n%r'%(response.code, response.payload))
 
       return response.payload
+    
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
