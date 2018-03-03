0b159c1bd288b3d2a6cf3a13a4cdc9653a845d12
ashproject <33730158+ashproject@users.noreply.github.com>
2018-03-03 18:02:48 -0500

    Update capturePicture.py

--- capturePicture.py
+++ capturePicture.py
@@ -27,22 +27,22 @@ def main(fromServer):
         picName = picT + '.jpg'
         FilePathPic = filePath + picName
         
-        motionState = picammotion.motion()
-        print(motionState)
-        if motionState:
+        #motionState = picammotion.motion()
+        #print(motionState)
+        #if motionState:
             # Take picture
-            with picamera.PiCamera() as camera:
-                camera.resolution = (1280,720)
-                
-                #Timestamp
-                #camera.annotate = 700+650
-                camera.annotate_foreground = Color('black')
-                camera.annotate_background = Color('white')
-                camera.annotate_text = picT
-                camera.capture(FilePathPic)
-                print("picture taken.")
-                
-                picCount += 1
-                sleep(3)
-                
-                
+        with picamera.PiCamera() as camera:
+            camera.resolution = (1280,720)
+
+            #Timestamp
+            #camera.annotate = 700+650
+            camera.annotate_foreground = Color('black')
+            camera.annotate_background = Color('white')
+            camera.annotate_text = picT
+            camera.capture(FilePathPic)
+            print("picture taken.")
+
+            picCount += 1
+            sleep(3)
+
+
