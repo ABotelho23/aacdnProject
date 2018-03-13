8503e6803a8f20945e7d80da24e3837a78807bff
ABotelho23 <botelho2305@gmail.com>
2018-03-13 14:51:23 -0400

    indent

--- hubAiocoap.py
+++ hubAiocoap.py
@@ -114,7 +114,7 @@ def testThread(loop):
 	"""This thread emulates what would be the GUI in the final product"""
 	print("TEST THREAD DEBUG #1: ",threading.current_thread())
 
-	"""this might return a future before it is available, CHECK THIS"""
+    """this might return a future before it is available, CHECK THIS"""
     packet = asyncio.run_coroutine_threadsafe(createRequest('GET', '10.0.0.101', '/bulb/colours'), loop)
 
     print("\n\n!==========REPONSE FROM NODE: ",packet,"==========!\n\n")
