402921a97c37c146dfe38dadaffe1cfecda09f53
ashproject <33730158+ashproject@users.noreply.github.com>
2018-03-16 16:27:09 -0400

    Add Resource
    Add Resources for pic and video

--- node.py
+++ node.py
@@ -21,7 +21,49 @@ class TestResource(resource.Resource):
     #this is the render for a GET. This returns the payload.
     async def render_get(self, request):
         return aiocoap.Message(payload=self.content)
+class TakePicture(resource.Resource):
+    """This is our first resource defined from scratch to test functionality."""
 
+    def __init__(self):
+        super().__init__()
+        self.set_content(b"If you perform a PUT on this URI, camera will take a picture.")
+
+    def set_content(self, content):
+        self.content = content
+
+    #this is the render for a GET. This returns the payload.
+    async def render_get(self, request):
+        return aiocoap.Message(payload=self.content)
+
+    #this is the render for a PUT. This sets what the resource value is.
+    async def render_put(self, request):
+        """print('PUT payload: %s' % request.payload)
+        self.set_content(request.payload)"""
+        #Call Take picture function
+        takePicture(request.payload)
+        return aiocoap.Message(code=aiocoap.CHANGED, payload=b'Picture captured.')
+
+class TakeVideo(resource.Resource):
+    """This is our first resource defined from scratch to test functionality."""
+
+    def __init__(self):
+        super().__init__()
+        self.set_content(b"If you perform a PUT on this URI, camera will take a video of the amount of seconds you send.")
+
+    def set_content(self, content):
+        self.content = content
+
+    #this is the render for a GET. This returns the payload.
+    async def render_get(self, request):
+        return aiocoap.Message(payload=self.content)
+
+    #this is the render for a PUT. This sets what the resource value is.
+    async def render_put(self, request):
+        """print('PUT payload: %s' % request.payload)
+        self.set_content(request.payload)"""
+        takeVideo(request.payload)
+        return aiocoap.Message(code=aiocoap.CHANGED, payload=b'Video captured.')
+# logging setup
 
 async def createRequest(request_type, node_address, node_resource, userPayload, protocol):
     if (request_type == 'GET'):
@@ -83,8 +125,8 @@ def main():
     root = resource.Site()
     root.add_resource(('.well-known', 'core'),
         resource.WKCResource(root.get_resources_as_linkheader))
-    root.add_resource(('camera','capture'), TakePicture())
-    root.add_resource(('camera','captureVideo'), TakeVideo())
+    root.add_resource(('picture'), TakePicture())
+    root.add_resource(('video'), TakeVideo())
 
     protocol = coap_loop.run_until_complete(aiocoap.Context.create_server_context(root))
 
