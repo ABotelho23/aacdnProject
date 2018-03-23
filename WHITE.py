909f46294e080dd46d5288af0eaf872caa4112dc
Nathan132 <33666107+Nathan132@users.noreply.github.com>
2018-03-23 12:32:43 -0400

    Create WHITE.py

---
+++ WHITE.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+
+def whiteOn():
+  GPIO.setmode(GPIO.BCM)
+  
+  RED = 17
+  GREEN = 18
+  BLUE = 27
+  GPIO.setup(RED,GPIO.OUT)
+  GPIO.setup(GREEN,GPIO.OUT)
+  GPIO.setup(BLUE,GPIO.OUT)
+  GPIO.output(RED,GPIO.LOW)
+  GPIO.output(GREEN,GPIO.LOW)
+  GPIO.output(BLUE,GPIO.LOW)
+  
+if __name__ == "__main__":
+  whiteOn()
