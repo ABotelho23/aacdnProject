6fb98c03e4566ec06e0d7fb030f7f0f008ea8392
thekidd54 <kidd.dan15@gmail.com>
2018-03-26 20:29:16 -0400

    Moved Gallery & Index CSS and JS to seperate files -Had to leave modal scripts in index -

---
+++ static/gallery.js
@@ -0,0 +1,124 @@
+
+//Picture Script
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      document.querySelector('#carousel-btn-first').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'first'}});
+        document.querySelector('#mdlext-carousel-demo2').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-scroll-prev').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'scroll-prev'}});
+        document.querySelector('#mdlext-carousel-demo2').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-prev').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'prev'}});
+        document.querySelector('#mdlext-carousel-demo2').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-next').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'next'}});
+        document.querySelector('#mdlext-carousel-demo2').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-scroll-next').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'scroll-next'}});
+        document.querySelector('#mdlext-carousel-demo2').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-last').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'last'}});
+        document.querySelector('#mdlext-carousel-demo2').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-play-pause').addEventListener('click', function (e) {
+        // Toggle play icon
+        var i = this.querySelector('i');
+        var action = i.innerText === 'play_circle_outline' ? 'play' : 'pause';
+        i.textContent = action === 'play' ? 'pause_circle_outline' : 'play_circle_outline';
+        var ev = new CustomEvent('command', {detail: {action: action, interval: 3000}});
+        document.querySelector('#mdlext-carousel-demo2').dispatchEvent(ev);
+      });
+      document.querySelector('#mdlext-carousel-demo2').addEventListener('select', function (e) {
+        if ('pause' === e.detail.command) {
+          // Set play icon
+          var i = document.querySelector('#carousel-btn-play-pause i');
+          i.textContent = 'play_circle_outline';
+        }
+        else {
+          var oldImage = document.querySelector('#carousel-imgviewer img');
+          var selectImage = e.detail.source.querySelector('img');
+          var selectImageSrc = selectImage.src;
+          if (e.detail.source.querySelector('a')) {
+            selectImageSrc = e.detail.source.querySelector('a').href;
+          }
+          if (selectImageSrc !== oldImage.src) {
+            var newImage = oldImage.cloneNode(true);
+            newImage.src = selectImageSrc;
+            oldImage.parentNode.replaceChild(newImage, oldImage);
+            var title = document.querySelector('#carousel-viewer-title');
+            title.textContent = selectImage.title;
+          }
+        }
+      });
+    });
+  }());
+
+
+
+//Video Script
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      document.querySelector('#carousel-btn-first2').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'first'}});
+        document.querySelector('#mdlext-carousel-demo3').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-scroll-prev2').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'scroll-prev'}});
+        document.querySelector('#mdlext-carousel-demo3').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-prev2').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'prev'}});
+        document.querySelector('#mdlext-carousel-demo3').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-next2').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'next'}});
+        document.querySelector('#mdlext-carousel-demo3').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-scroll-next2').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'scroll-next'}});
+        document.querySelector('#mdlext-carousel-demo3').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-last2').addEventListener('click', function (e) {
+        var ev = new CustomEvent('command', {detail: {action: 'last'}});
+        document.querySelector('#mdlext-carousel-demo3').dispatchEvent(ev);
+      });
+      document.querySelector('#carousel-btn-play-pause2').addEventListener('click', function (e) {
+        // Toggle play icon
+        var i = this.querySelector('i');
+        var action = i.innerText === 'play_circle_outline' ? 'play' : 'pause';
+        i.textContent = action === 'play' ? 'pause_circle_outline' : 'play_circle_outline';
+        var ev = new CustomEvent('command', {detail: {action: action, interval: 3000}});
+        document.querySelector('#mdlext-carousel-demo3').dispatchEvent(ev);
+      });
+      document.querySelector('#mdlext-carousel-demo3').addEventListener('select', function (e) {
+        if ('pause' === e.detail.command) {
+          // Set play icon
+          var i = document.querySelector('#carousel-btn-play-pause2 i');
+          i.textContent = 'play_circle_outline';
+        }
+        else {
+          var oldImage = document.querySelector('#carousel-imgviewer2 img');
+          var selectImage = e.detail.source.querySelector('img');
+          var selectImageSrc = selectImage.src;
+          if (e.detail.source.querySelector('a')) {
+            selectImageSrc = e.detail.source.querySelector('a').href;
+          }
+          if (selectImageSrc !== oldImage.src) {
+            var newImage = oldImage.cloneNode(true);
+            newImage.src = selectImageSrc;
+            oldImage.parentNode.replaceChild(newImage, oldImage);
+            var title = document.querySelector('#carousel-viewer-title2');
+            title.textContent = selectImage.title;
+          }
+        }
+      });
+    });
+  }());
---
+++ static/index.js
@@ -0,0 +1,140 @@
+// <!-- Update temp on page load-->
+    $(function() {
+     $('a#CurrentTemp').ready(function() {
+      $.getJSON('/tempbackground_proc',
+        function(data) {
+            $("#CurrentTemp").text(data.result);
+        });
+        return false;
+    });
+});
+
+// <!-- Update temp on refresh button-->
+    $(function() {
+     $('a#tempUpdate').bind('click',function() {
+      $.getJSON('/tempbackground_proc',
+        function(data) {
+            $("#CurrentTemp").text(data.result);
+        });
+        return false;
+    });
+});
+
+// <!-- Update blind on page load/refresh button-->
+    $(function() {
+     $('a#CurrentBlind').ready(function() {
+      $.getJSON('/blindbackground_proc',
+        function(data) {
+            $("#CurrentBlind").text(data.result);
+        });
+        return false;
+    });
+});
+    $(function() {
+     $('a#blindUpdate').bind('click', function() {
+      $.getJSON('/blindbackground_proc',
+        function(data) {
+            $("#CurrentBlind").text(data.result);
+        });
+        return false;
+    });
+});
+
+// <!-- Update bulb on page load/refresh button-->
+    $(function() {
+     $('a#CurrentLight').ready(function() {
+      $.getJSON('/bulbbackground_proc',
+        function(data) {
+            $("#CurrentLight").text(data.result);
+        });
+        return false;
+    });
+});
+    $(function() {
+     $('a#lightUpdate').bind('click', function() {
+      $.getJSON('/bulbbackground_proc',
+        function(data) {
+            $("#CurrentLight").text(data.result);
+        });
+        return false;
+    });
+});
+
+// <!--- Simple Function for Slider Display --->
+function outputUpdate(vol) {
+    if(vol == '0'){
+        document.querySelector('#blindSliderLabel').value = 'Fully Open';
+    } else if (vol == '10'){
+        document.querySelector('#blindSliderLabel').value = 'Fully Closed';
+    } else {
+        document.querySelector('#blindSliderLabel').value = vol + '0% Closed';
+    }
+}
+
+
+//Function for bulb control with buttons
+$(function() {
+$('.ButtonTest').on('click', function() {
+$.getJSON('/bulbsetbackground_proc', {
+    colourVal: $(this).attr('value'),
+    onoffVal: ('on')
+  },
+  function(data) {
+      $("#result2").text(data.result);
+  });
+  return false;
+});
+});
+
+//Function for bulb control off button
+$(function() {
+$('#Coff').on('click', function() {
+$.getJSON('/bulbsetbackground_proc', {
+    colourVal: $('button[name="changeColour"]').val(),
+    onoffVal: $('button[name="onoffValue"]').val()
+  },
+  function(data) {
+      $("#result2").text(data.result);
+  });
+  return false;
+});
+});
+
+//Function for blind control slider
+$(function() {
+$('a#blindSlider').on('click', function() {
+$.getJSON('/blindsetbackground_proc', {
+    blindVal: $('input[name="blindValue"]').val(),
+  },
+  function(data) {
+      $("#result2").text(data.result);
+  });
+  return false;
+});
+});
+
+//Function for picture control
+$(function() {
+$('#pSend').on('click', function() {
+$.getJSON('/picturebackground_proc', {
+    picVal: $('input[id="picture"]').val(),
+  },
+  function(data) {
+      $("#result2").text(data.result);
+  });
+  return false;
+});
+});
+
+//Function for video control
+$(function() {
+$('#vSend').on('click', function() {
+$.getJSON('/videobackground_proc', {
+    vidVal: $('input[id="video"]').val(),
+  },
+  function(data) {
+      $("#result2").text(data.result);
+  });
+  return false;
+});
+});
