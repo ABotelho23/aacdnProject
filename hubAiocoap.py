fcec90af85cb468c69997e16768d8d981e6779f7
connormoss <33731342+connormoss@users.noreply.github.com>
2018-03-28 15:41:33 -0400

    Decimal placement attempt two

--- hubAiocoap.py
+++ hubAiocoap.py
@@ -38,8 +38,7 @@ def multiPage():
 @app.route("/tempbackground_proc")
 def checkTemp():
     currentTemp = asyncio.run_coroutine_threadsafe(createRequest('GET', '10.0.0.103', '/thermo/temp','0', flaskProtocol), flaskLoop).result()
-    currentTemp = str(currentTemp)
-    currentTemp[2] = '.'
+    currentTemp = currentTemp/1000
     return jsonify(result=currentTemp)
 
 @app.route("/bulbbackground_proc")
