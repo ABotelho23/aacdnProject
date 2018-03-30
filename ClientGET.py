1514703b76869536b543a66e7d67955cb4bffc3a
ABotelho23 <botelho2305@gmail.com>
2018-03-30 14:33:06 -0400

    update clients for testing

--- ClientGET.py
+++ ClientGET.py
@@ -6,9 +6,15 @@ from aiocoap import *
 logging.basicConfig(level=logging.INFO)
 
 async def main():
+
+    addressIn = input("Target?")
+    resourceIn = input ("Resource? (Include initial slash, and no ending slash.)")
+
     protocol = await Context.create_client_context()
 
-    request = Message(code=GET, uri='coap://10.0.0.101/test')
+    targetURI = 'coap://' + addressIn + resourceIn
+
+    request = Message(code=GET, uri=targetURI)
 
     try:
         response = await protocol.request(request).response
--- ClientPUT.py
+++ ClientPUT.py
@@ -6,20 +6,18 @@ from aiocoap import *
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    """Perform a single PUT request to localhost on the default port, URI
-    "/other/block".
-    The payload is bigger than 1kB, and thus sent as several blocks."""
 
-    context = await Context.create_client_context()
+    addressIn = input("Target?")
+    resourceIn = input ("Resource? (Include initial slash, and no ending slash.)")
+    payloadIn = input("Payload?")
 
-    payload = b"Hello.\n"
-    request = Message(code=PUT, payload=payload)
-    # These direct assignments are an alternative to setting the URI like in
-    # the GET example:
-    request.opt.uri_host = '10.0.0.101'
-    request.opt.uri_path = ("other","block")
+    protocol = await Context.create_client_context()
 
-    response = await context.request(request).response
+    targetURI = 'coap://' + addressIn + resourceIn
+
+    request = Message(code=PUT, uri=targetURI, payload=payloadIn)
+
+    response = await protocol.request(request).response
 
     print('Result: %s\n%r'%(response.code, response.payload))
 
