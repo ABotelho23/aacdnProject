83e858b0f438ee66a4ac6226ce424ba821d1ff4a
thekidd54 <kidd.dan15@gmail.com>
2018-03-30 15:48:03 -0400

    Test

--- nodeCode.py
+++ nodeCode.py
@@ -28,10 +28,10 @@ class MovementResource(resource.Resource):
     #this is the render for a PUT. This sets what the resource value is.
     async def render_put(self, request):
         print('PUT payload: %s' % request.payload)
-        aiocoap.Message(code=aiocoap.EMPTY, payload='test')
-        self.set_content(request.payload)
-        activateMotor(request.payload)
-        return aiocoap.Message(code=aiocoap.CHANGED, payload=self.content)
+        await: 
+            self.set_content(request.payload)
+            activateMotor(request.payload)
+            return aiocoap.Message(code=aiocoap.CHANGED, payload=self.content)
 
 #logging setup
 logging.basicConfig(level=logging.INFO)
