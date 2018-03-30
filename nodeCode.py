d32a15877819639e2e5f0e95ad292920a109c9ee
connormoss <33731342+connormoss@users.noreply.github.com>
2018-03-30 13:53:56 -0400

    Update nodeCode.py

--- nodeCode.py
+++ nodeCode.py
@@ -29,7 +29,7 @@ class Temperature(resource.Resource):
         y = x[2]
         z = int(y)
         z = z/1000
-        print (z)
+        print(z)
         #encode to string for payload
         tempvalue = z.encode()
         self.set_content(tempvalue)
