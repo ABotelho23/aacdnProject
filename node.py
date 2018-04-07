161495162047762de85e6c449d05bf1723af02ba
ABotelho23 <botelho2305@gmail.com>
2018-04-07 19:30:14 -0400

    run coroutine thread safe

--- node.py
+++ node.py
@@ -116,7 +116,7 @@ def backgroundTask(loop,protocol):
         #self.mode = b'0'
     time.sleep(5)
     while True:
-            asyncio.Task(motion(loop,protocol))
+            asyncio.run_coroutine_threadsafe(motion(loop,protocol),loop))
 
 def main():
 
