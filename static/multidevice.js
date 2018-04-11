8e82d71ba8cc08ea15f8ab203b10b903c1f3fc12
thekidd54 <kidd.dan15@gmail.com>
2018-04-11 19:18:48 -0400

    PartyMode Change to Rainbow -Cycles thru all colours -Takes 1 Picture

--- static/multidevice.js
+++ static/multidevice.js
@@ -37,41 +37,21 @@ $.getJSON('/blindsetbackground_proc', {
 });
 });
 
-
-var colours = ["Red", "Green", "Blue", "Magenta", "Yellow", "Cyan", "White"];
-function setNightmare(){
-    var x = Math.floor((Math.random()*7) + 0);
-    var y = Math.floor((Math.random()*11) + 0);
-    var z = Math.floor((Math.random()*3) + 1);
-        //Sets Bulb random colour
-        $.getJSON('/bulbsetbackground_proc', {
-            colourVal: (colours[x]),
-            onoffVal: ('On')
-          },
-          function(data) {
-              $("#result2").text(data.result);
-          });
-          //Sets Blind random height (0-10)
-        $.getJSON('/blindsetbackground_proc', {
-            blindVal: (y),
-         },
-         function(data) {
-              $("#result2").text(data.result);
-          });
-          //Takes 1-3 pictures with camera
-        $.getJSON('/picturebackground_proc', {
-            picVal: (z),
-         },
-         function(data) {
-            $("#result2").text(data.result);
-         });
-}
 //Party Mode Activate
 function PartyModeAct(){
-    for (var i = 0; i < 1; i++) {
-
-        setInterval(setNightmare, 5000);
-
-    }
+    //Cycle Colours
+    $.getJSON('/bulbsetbackground_proc', {
+        colourVal: ('Rainbow'),
+        onoffVal: ('On')
+      },
+      function(data) {
+          $("#result2").text(data.result);
+      });    
+    $.getJSON('/picturebackground_proc', {
+        picVal: (1),
+     },
+     function(data) {
+        $("#result2").text(data.result);
+     });
 
 }
