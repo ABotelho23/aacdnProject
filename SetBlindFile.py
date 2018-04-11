5a106e3fb756cd780583adc3113987600fd10b2f
thekidd54 <kidd.dan15@gmail.com>
2018-04-11 19:56:58 -0400

    Fixed SetBlindFile

--- SetBlindFile.py
+++ SetBlindFile.py
@@ -1,12 +1,20 @@
 #Library Imports
 import RPi.GPIO as GPIO
 from time import sleep
-import blindStatus
+
+def writeStatus(num):
+
+  #Update Status File
+  print ("Writting status to file")
+  writePath = '/home/pi/BlindStatus.txt'
+  statusFile = open(writePath, 'w')
+  statusFile.write(num)
+  statusFile.close()
 
 def main():
 
     setBlind = input("Enter Value...")
-    blindStatus.writeStatus(str(setBlind))
+    writeStatus(str(setBlind))
 
 if __name__ == "__main__":
     main()
